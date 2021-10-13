75d8801874d26340132195c8f12ec4ffddfc79d3
Vignesh <joshivignesh@users.noreply.github.com>
2021-10-13 14:01:36 +0530

    Added code to display content
    Added code to display content

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.blogs');
+
+const renderposts = async () => {
+    let uri = 'http://localhost:3000/posts';
+    const posts = await (await fetch(uri)).json();
+
+    let template ='';
+    posts.forEach(element => {
+        template+= `
+        <div class="post">
+        <h2>${element.title}</h2>
+        <p><small>${element.likes} likes </small></p>
+        <p>${element.body}</p>
+        <a href="/details.html">read more...</a>
+        </div>
+        `
+    });
+
+    container.innerHTML = template;
+
+}
+
+window.addEventListener('DOMContentLoaded', ()=> renderposts());
